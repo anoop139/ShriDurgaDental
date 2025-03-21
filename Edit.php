df8c35f098a3b2c2876c9b8de54496f5534de19b
Anoop <anoopsrhetty@gmail.com>
2025-03-21 20:42:50 +0530

    Work dene on 21-03-25

---
+++ Edit.php
@@ -0,0 +1,16 @@
+<?php
+include("Connection/Connect.php");
+error_reporting(0);
+$name = $_GET['n'];
+?>
+<html>
+<head>
+</head>
+<body>
+<body>
+<?php
+$id = $_GET['id'];
+$name2 = $_GET['name'];
+
+?>
+</html>
