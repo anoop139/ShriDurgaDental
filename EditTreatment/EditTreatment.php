a879d72e0e3f4cb95c1a15c8089307404e967842
Anoop <anoopsrhetty@gmail.com>
2025-06-02 21:48:17 +0530

    Added new folder

---
+++ EditTreatment/EditTreatment.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="javascriptract">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="stylesheet" href="../Header.css?v=7">
+    <style>
+        #main{
+            border: 2px solid black;
+            padding: 200px; 
+        }   
+        #main{
+            padding-top:100px;
+            padding-left:800px;
+        }
+           #main{
+            margin-top:20px ; 
+        }
+    </style>
+    <title>Document</title>
+</head>
+<body>
+<div id="header0" style="background-color:lightblue">
+<div id="header">
+
+        <ul>
+        <li><a href="DentalHomePage.html">Home </a></li>&nbsp;
+        <li><a href="PatientFom.html">Add Patient </a></li>&nbsp;
+        
+        <li><a href="SearchByName.php">Search by Name</a></li>
+        </ul>
+    </div>
+    </div>
+    <div id="main">
+        <table border="2" cellpadding="4px">
+            <tr>
+            <th>Treatment name</th>
+            <th>Edit Treatment name</th>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
