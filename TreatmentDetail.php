c78b73b5950d5715fe1b376ac19867d6d3fec6af
Anoop <anoopsrhetty@gmail.com>
2025-07-30 20:34:36 +0530

    increased padding for treatment feild

--- TreatmentDetail.php
+++ TreatmentDetail.php
@@ -177,7 +177,7 @@ while( $fect= mysqli_fetch_assoc($query))
 {
 	echo"<tr>
    <td>$fect[date]</td>
-   <td  style='padding: 10px;'>$fect[treatment]</td>
+   <td style='padding: 10px;'>$fect[treatment]</td>
    <td>$fect[amount]</td>
   <td><a href='EditTreatment\EditTreatment.php?tid=$fect[tid]'>Edit</a></td>
   <td><a href='Edit\TreatmentDelete.php?id=$PatienName[sno]&treatId=$fect[tid]'>Delete</a></td>
