f6b51db4c8aa170d2f00b2716075ddf52de44254
Anoop <anoopsrhetty@gmail.com>
2025-11-06 21:47:01 +0530

    in process

--- Export.php
+++ Export.php
@@ -16,11 +16,11 @@ error_reporting(0);
      padding: 50px 50px;
   }
   #buttons{
-    text-align:right
+    position:absolute;
+    top     :  10px;
+    left:      300px;
   }
-   table tr th, td{
-        padding: 5px;
-      }
+  
 </style>
 <body>
     <script>
@@ -35,57 +35,77 @@ error_reporting(0);
     </script>
     <div id="contain">
        <?php
-        if (isset($_POST['p'])) {
+      if (isset($_POST['p'])) {
+       
             $getDate  = $_POST["date"];        
             $getDate2 =$getDate;
-        $todayRecord ="SELECT * FROM patient where date = '$getDate'";
+        $todayRecord ="SELECT * FROM patient where date = '6 - 11 - 2025'";
         $dateQuery   = mysqli_query($conn, $todayRecord);
-        $count       = mysqli_num_rows($dateQuery);
+        $count    = mysqli_num_rows($dateQuery);
         $html;//
         if ($count==0) {
             # code...
             echo"<h1 style='text-align:center'>No record for the day </h1>";
         }
         else{
-            $html="
-               <table border='2'>
-             <tr>
-             <th>Date</th>
-             <th>Name</th>
-             <th>Age</th>
+         $html="
+            <table border='2' style='margin-left:120px' id='table1'>
+              <tr>
+              <td></td>
+              <th colspan='4'>Patient details</th>
+             </tr>
+               <tr>
+               <td></td>z
+                <th>Date</th>
+                <th>Name</th>
+              <th>Age</th>
              <th>Phone Number</th>
              </tr>";
           while( $fetch = mysqli_fetch_assoc($dateQuery))
           {
-            $html.=" <tr>
+            $html.="<tr>
+            <td></td>
+
              <th>$fetch[date]</th>
              <th>$fetch[name]</th>
              <th>$fetch[age]</th>
              <th>$fetch[phoNo]</th>
-             </tr>
+               </tr>
             ";
           }
-            $html."</table>";
+        echo$html;//;/
 
-              header("content-Type:application/vnd.ms-excel");
-       header("content-disposition:attachment;filename=$getDate.xls");
-         echo$html;
-         exit;
+       // //  echo'hello';
+        //  exit;
         }
   
-    
-    }
+  
+      }
      
        ?>
-       
     </div>
 	<!-- <h1>New file</h1> -->
- <form action="#" method="POST">
+ <form action="#"  method="POST">
     <input type="hidden" name="date" id="dateId" value='test'>
      <div id="buttons">
-        <button name="p">Today's record</button>
+        <input type="submit" name='p' value="Download">
+        <button  onclick="expotToExcel()">Today's record</button>
 
     </div>
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js"></script>
+   <script>
+   function expotToExcel()
+   {
+    let table = document.getElementById("table1");
+    let fp   = XLSX.utils.table_to_book(table, {sheet:'sheet1'})
+    XLSX.write(fp, {
+      bookType:"xlsx",
+      type    :"base64"
+    })
+    XLSX.writeFile(fp, 'test.xlsx')
+    alert("test the ")
+   } 
+   </script>
 </body>
 </html>
