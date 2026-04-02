5ad0926342cf4e246e9f89c2d84bda62e7adda91
Anoop <anoopsrhetty@gmail.com>
2026-04-02 21:41:26 +0530

    Almost secured

--- Session.php
+++ Session.php
@@ -3,14 +3,19 @@ session_start();
 include("Connection/Connect.php");
 
 $username = $_POST['username'];
-$password = $_POST['password'];
-
-$query = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
-$result = mysqli_query($conn, $query);
-
-if(mysqli_num_rows($result) == 1){
+// $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$query = "SELECT * FROM admin WHERE username=?";
+$prepare = mysqli_prepare($conn, $query);
+mysqli_stmt_bind_param($prepare, 's',$username);
+mysqli_stmt_execute($prepare);
+$result = mysqli_stmt_get_result($prepare);
+$count    = mysqli_num_rows($result);
+// mysqli_stmt_close($prepare);
+if($count==1){
 
     $row = mysqli_fetch_assoc($result);   // 🔥 THIS WAS MISSING
+if(password_verify($_POST['password'], $row['password'])){    
+    session_regenerate_id(true); // ✅ HERE
 
     $_SESSION['user'] = $row['username'];
     $_SESSION['admin_id'] = $row['id'];
@@ -19,7 +24,14 @@ if(mysqli_num_rows($result) == 1){
 
     exit();
 
-} else {
+}
+ else {
+    header("Location: login.php?error=1");
+    exit();
+}
+
+}
+else {
     header("Location: login.php?error=1");
     exit();
 }
