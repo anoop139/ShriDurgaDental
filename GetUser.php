3f372ddb016bff5dd849eaf6c3972cdaad65f03c
Anoop <anoopsrhetty@gmail.com>
2026-04-27 18:58:21 +0530

    Almost secured

--- GetUser.php
+++ GetUser.php
@@ -3,8 +3,7 @@ session_start();
 
 // Check if user is logged in
 if(isset($_SESSION['user'])){
-    echo $_SESSION['user'];
-} else {
-    echo "Not logged in";
-}
+      echo htmlspecialchars($_SESSION['user'], ENT_QUOTES, 'UTF-8');
+
+} 
 ?>
