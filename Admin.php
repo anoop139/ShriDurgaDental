e02f93171ec8441c41a5b6d5dcc11488197423b0
Anoop <anoopsrhetty@gmail.com>
2026-05-07 22:10:33 +0530

    Almost secured

--- Admin.php
+++ Admin.php
@@ -3,9 +3,8 @@ include("Connection/Connect.php");
 
 $username = "admin";
 $password = password_hash("admin123", PASSWORD_DEFAULT);
-
-$query = "INSERT INTO admin (username, password) VALUES ('$username', '$password')";
-mysqli_query($conn, $query);
-
+$stmt = mysqli_prepare($conn, "INSERT INTO admin (username, password) VALUES (?, ?)");
+mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+mysqli_stmt_execute($stmt);
 echo "Admin created successfully!";
 ?>
