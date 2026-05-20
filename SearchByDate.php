004aa23cfec8e9cf021ea5b860609acc0979f8a7
Anoop <anoopsrhetty@gmail.com>
2026-05-20 21:32:22 +0530

    Almost secured

--- SearchByDate.php
+++ SearchByDate.php
@@ -4,7 +4,9 @@ ini_set('log_errors', 1);
 ini_set('display_errors', 0);
 error_reporting(E_ALL);
 ini_set('session.cookie_httponly', 1);
-ini_set('session.cookie_secure', 0);
+ini_set('session.cookie_secure',
+    (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 1 : 0
+);
 ini_set('session.cookie_samesite', 'Strict');
 session_start();
 if (!isset($_SESSION['token'])) {
