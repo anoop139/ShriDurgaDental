132d74b2c4fc1565d4639387e744a0f2c7e94a75
Anoop <anoopsrhetty@gmail.com>
2026-05-22 21:24:59 +0530

    Almost secured

--- SearchByNumber.php
+++ SearchByNumber.php
@@ -1,6 +1,14 @@
 <?php
 include("Connection/Connect.php");
-error_reporting(0);
+include("Connection/Init.php");
+if(!isset($_SESSION['user'])){
+    header("Location: LogIn.php");
+    exit();
+}
+$admin_id = $_SESSION['admin_id'];
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -83,10 +91,10 @@ error_reporting(0);
 		padding-left:308px;
 	}
 	
-	#Number
-	{
-		text-docoration:none;
-	}
+#Number
+{
+    text-decoration: none;
+}
 	#TreatInserted
 	 {
 		position: absolute;
@@ -119,15 +127,15 @@ error_reporting(0);
     <link rel="stylesheet" href="Header2.css">
 </head>
 <body>
-       <ul style="background:black; height: 40px; width:340px; padding-left:1000px"  >
+        <ul style="background:black; height: 40px; width:340px; padding-left:1000px">
         <li><a href="DentalHomePage.php">Home </a></li>&nbsp;
-        <li><a href="http://localhost:8081/Shri/PatientRecord.php">Patient List </a></li>&nbsp;
-        <li><a href="http://localhost:8081/Shri/PatientFom.html">Add Patient </a></li>&nbsp;
+        <li><a href="PatientRecord.php">Patient List </a></li>&nbsp;
+        <li><a href="PatientFom.php">Add Patient </a></li>&nbsp;
         
         <li><a href="">Search by</a>
    <ul>
-            <li><a href="http://localhost:8081/Shri/SearchByDate.php">Date</a></li><br>
-            <li><a href="http://localhost:8081/Shri/SearchByName.php">Name</a></li>
+            <li><a href="SearchByDate.php">Date</a></li><br>
+            <li><a href="SearchByNumber.php">Number</a></li>
     
         </ul>
         </li>
@@ -137,23 +145,33 @@ error_reporting(0);
 <div id="res1">
 <h1>Search by Number :</h1>
 <form id="input"onsubmit="return checkNumberInput()" method="POST">
+<input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>">
 <input type="text" id="input1" name="number" class="Col">&nbsp;
 <input type="submit" name="Sub" class="Col" value="Click here" ><br>
 </form><br><br>
 <div id="resultDiv">
 		<?php
-	if(isset($_POST['Sub']))
+	 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
-{    
-	$number =  $_POST['number'];
-
-// 
-   $patientInfo = "SELECT * FROM patient WHERE phoNo='$number'";
-
-	$query       = mysqli_query($conn, $patientInfo);
-	$no           = mysqli_num_rows($query);
-	 
+{   
+if (!isset($_POST['token']) || !hash_equals($_SESSION['token'], $_POST['token'])) {
+    exit("Invalid request");
+}
 	
+$number = trim($_POST['number']);
+
+
+if (!preg_match("/^[0-9]{10,15}$/", $number)) {
+    exit("Invalid number");
+}
+  $patientInfo = "SELECT * FROM patient WHERE phoNo=? AND admin_id=?";
+   $prepared    = mysqli_prepare($conn, $patientInfo);
+   mysqli_stmt_bind_param($prepared, "si", $number, $admin_id);
+   mysqli_stmt_execute($prepared);
+$result       = mysqli_stmt_get_result($prepared);
+	$no           = mysqli_num_rows($result);
+	//  echo"<h1>No. ".$no."</h1>";
+	mysqli_stmt_close($prepared);
 	
 	if($no>0)
 	{
@@ -169,21 +187,26 @@ error_reporting(0);
 	 <th style='padding:5px;'>Phone Number</th>
 	 <th style='padding:5px;'>Edit</th>
 	 </tr>";
-      while($fetch =mysqli_fetch_assoc($query))
-	  { 
-        $id ="select * from treatment where sno=$fetch[sno]";
-		$query2       = mysqli_query($conn, $id);
-	    $no2           = mysqli_num_rows($query2);   
-		 ///  echo"hi $fetch[sno]";
+      while($fetch =mysqli_fetch_assoc($result))
+	  {
+		
+	  $id ="SELECT COUNT(*) as total FROM treatment WHERE sno=?";
+		$prepared2      = mysqli_prepare($conn, $id);
+		mysqli_stmt_bind_param($prepared2, 'i', $fetch['sno']);
+		mysqli_stmt_execute($prepared2);
+		$numberOfTreat = mysqli_stmt_get_result($prepared2);
+		$count = mysqli_fetch_assoc($numberOfTreat);
+  $no2 = $count['total']?? 0; //mysqli_num_rows($res); 
+	mysqli_stmt_close($prepared2);
            		echo"<tr>
-		  <td class='td'>$fetch[date]</td>
-		  <td class='td'>$fetch[name]</td>
-	 <td style='text-align:center;' class='td'>$fetch[age]</td>
-	 <td style='text-align:center' class='td'>$fetch[gen]</td>
-	 <td style='text-align:center' class='td'><a id='Number' href='TreatmentDetail.php?id=$fetch[sno]'>$no2</a></td>
-	 <td style='text-align:center' class='td'><a id='Number' href='InsertTreatment.php?id=$fetch[sno]&sbm=True'>Click here to add treatment</a></td>
-	 <td class='td'>$fetch[phoNo]</td>
-	 <td class='td'><a href='Edit.php?id=$fetch[sno]'>Edit</a></td>
+		  	  <td class='td'>".htmlspecialchars($fetch['date'], ENT_QUOTES, 'UTF-8')."</td>
+		  <td class='td'>".htmlspecialchars($fetch['name'], ENT_QUOTES, 'UTF-8')."</td>
+	 <td style='text-align:center;' class='td'>".htmlspecialchars($fetch['age'], ENT_QUOTES, 'UTF-8')."</td>
+	 <td style='text-align:center' class='td'>".htmlspecialchars($fetch['gen'], ENT_QUOTES, 'UTF-8')."</td>
+	 <td style='text-align:center' class='td'><a id='Number' href='TreatmentDetail.php?id=".htmlspecialchars($fetch['sno'], ENT_QUOTES, 'UTF-8')."'>$no2</a></td>
+	 <td style='text-align:center' class='td'><a id='Number' href='InsertTreatment.php?id=".htmlspecialchars($fetch['sno'], ENT_QUOTES, 'UTF-8')."&sbm=True'>Click here to add treatment</a></td>
+<td class='td'>".htmlspecialchars($fetch['phoNo'], ENT_QUOTES, 'UTF-8')."</td>
+	 <td class='td'><a href='Edit.php?id=".htmlspecialchars($fetch['sno'], ENT_QUOTES, 'UTF-8')."'>Edit</a></td>
 	 </tr>";		  
 	  }
        echo"</table>";
@@ -194,57 +217,57 @@ error_reporting(0);
 	}
 
 }
-else if(isset($_GET["pid"]))
-{
-	$name1 =  $_GET['name4'];
-	$pid = $_GET["pid"];
+// else if(isset($_GET["pid"]))
+// {
+// 	$name1 =  $_GET['name4'];
+// 	$pid = $_GET["pid"];
 
-// 
-   $patientInfo = "SELECT * FROM patient WHERE sno =$pid";
+// // 
+//    $patientInfo = "SELECT * FROM patient WHERE sno =$pid";
 
-	$query       = mysqli_query($conn, $patientInfo);
-	$no           = mysqli_num_rows($query);
+// 	$query       = mysqli_query($conn, $patientInfo);
+// 	$no           = mysqli_num_rows($query);
 	 
 	
 	
-	if($no>0)
-	{
+// 	if($no>0)
+// 	{
 		
-		echo" <table border='2'>
-	 <tr>
-	 <th style='padding:3px;'>Date</th>
-	 <th style='padding:3px;'>Name</th>
-	 <th style='padding:5px;'>Age</th>
-	 <th style='padding:5px;'>Gender</th>
-	 <th style='padding:5px;'>No. of treatment</th>
-	 <th style='padding:5px;'>Treatment details</th>
-	 <th style='padding:5px;'>Phone Number</th>
-	 <th style='padding:5px;'>Edit</th>
-	 </tr>";
-      while($fetch =mysqli_fetch_assoc($query))
-	  { 
-        $id ="select * from treatment where sno=$fetch[sno]";
-		$query2       = mysqli_query($conn, $id);
-	    $no2           = mysqli_num_rows($query2);   
-		 ///  echo"hi $fetch[sno]";
-           		echo"<tr>
-		  <td class='td'>$fetch[date]</td>
-		  <td class='td'>$fetch[name]</td>
-	 <td style='text-align:center;' class='td'>$fetch[age]</td>
-	 <td style='text-align:center' class='td'>$fetch[gen]</td>
-	 <td style='text-align:center' class='td'><a id='Number' href='TreatmentDetail.php?id=$fetch[sno]'>$no2</a></td>
-	 <td style='text-align:center' class='td'><a id='Number' href='InsertTreatment.php?id=$fetch[sno]&sbm=True'>Click here to add treatment</a></td>
-	 <td class='td'>$fetch[phoNo]</td>
-	 <td class='td'><a href='Edit.php?id=$fetch[sno]'>Edit</a></td>
-	 </tr>";		  
-	  }
-       echo"</table>";
-	}
-	else
-	{
-		echo"<h1 style='padding-left:350px;'>No recod found</h1>";
-	}
-	}
+// 		echo" <table border='2'>
+// 	 <tr>
+// 	 <th style='padding:3px;'>Date</th>
+// 	 <th style='padding:3px;'>Name</th>
+// 	 <th style='padding:5px;'>Age</th>
+// 	 <th style='padding:5px;'>Gender</th>
+// 	 <th style='padding:5px;'>No. of treatment</th>
+// 	 <th style='padding:5px;'>Treatment details</th>
+// 	 <th style='padding:5px;'>Phone Number</th>
+// 	 <th style='padding:5px;'>Edit</th>
+// 	 </tr>";
+//       while($fetch =mysqli_fetch_assoc($query))
+// 	  { 
+//         $id ="select * from treatment where sno=$fetch[sno]";
+// 		$query2       = mysqli_query($conn, $id);
+// 	    $no2           = mysqli_num_rows($query2);   
+// 		 ///  echo"hi $fetch[sno]";
+//            		echo"<tr>
+// 		  <td class='td'>$fetch[date]</td>
+// 		  <td class='td'>$fetch[name]</td>
+// 	 <td style='text-align:center;' class='td'>$fetch[age]</td>
+// 	 <td style='text-align:center' class='td'>$fetch[gen]</td>
+// 	 <td style='text-align:center' class='td'><a id='Number' href='TreatmentDetail.php?id=$fetch[sno]'>$no2</a></td>
+// 	 <td style='text-align:center' class='td'><a id='Number' href='InsertTreatment.php?id=$fetch[sno]&sbm=True'>Click here to add treatment</a></td>
+// 	 <td class='td'>$fetch[phoNo]</td>
+// 	 <td class='td'><a href='Edit.php?id=$fetch[sno]'>Edit</a></td>
+// 	 </tr>";		  
+// 	  }
+//        echo"</table>";
+// 	}
+// 	else
+// 	{
+// 		echo"<h1 style='padding-left:350px;'>No recod found</h1>";
+// 	}
+// 	}
 
 
 ?>
