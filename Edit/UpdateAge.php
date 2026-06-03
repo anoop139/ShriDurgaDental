5713600dd0337c184d2609ea7b8b23fffba7f237
Anoop <anoopsrhetty@gmail.com>
2026-06-03 19:28:32 +0530

    Almost secured 9/10

--- Edit/UpdateAge.php
+++ Edit/UpdateAge.php
@@ -1,8 +1,22 @@
 <?php
-include("../Connection/Connect.php");
-error_reporting(0);
-?>
+include("Connection/Connect.php");
 
+ini_set('log_errors', 1);
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+session_start();
+if (!isset($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
+}
+if (!isset($_SESSION['user']) || !isset($_SESSION['admin_id'])) {
+    header("Location: LogIn.php");
+    exit();
+}
+if (!isset($_POST['csrf_token']) || !hash_equals($_SESSION['token'], $_POST['csrf_token'])) {
+    die("Invalid CSRF token");
+}
+
+?>
 <!DOCTYPE html>
 <html lang="javascriptract">
 <head>
@@ -30,10 +44,21 @@ error_reporting(0);
     <div id="Main-div">
      <div style="text-align:center" id="sub-div">
         <?php
-        $id = $_POST['id'];// THIS IS IS SNO
-        $newAge = $_POST['newAge'];
-        $update ="update patient set age=$newAge where sno=$id";
-        $query = mysqli_query($conn, $update);
+
+        if (!isset($_POST['id']) || !isset($_POST['newAge'])) {
+            die("Invalid request");
+        }
+        $id = (int)$_POST['id'];// THIS IS IS SNO
+       $newAge = (int)$_POST['newAge'];
+        if ($newAge<=0 || $newAge>=120) {
+            die("Change the age to a valid one");
+        }
+
+        $update ="update patient set age=? where sno=?";
+            $stmt = mysqli_prepare($conn, $update);
+            mysqli_stmt_bind_param($stmt, "ii", $newAge, $id);
+            $query = mysqli_stmt_execute($stmt);
+        // $query = mysqli_query($conn, $update);
         if ($query) {
             # code...
             echo"<h1>Age updated successfully wait for few seconds </h1>";
@@ -48,7 +73,7 @@ error_reporting(0);
 
         <script>
       var up = setTimeout(() => {
-   window.location.href=`../Edit.php?newName=<?php echo$id;?>`
+   window.location.href=`../Edit.php?id=<?php echo$id;?>`
         }, 5000);
 
         </script>
