2e898edb92edecb5a14dbc33d9f278c43d4ff02b
Anoop <anoopsrhetty@gmail.com>
2026-06-12 18:13:39 +0530

    Almost secured 9.5/10

--- Edit/UpdateGen.php
+++ Edit/UpdateGen.php
@@ -18,11 +18,11 @@ $admin_id = $_SESSION['admin_id'];
 ?>
 
 <!DOCTYPE html>
-<html lang="javascriptract">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Update Gender</title>
     <style>
         #Main-div{
             border:2px solid black;
@@ -62,24 +62,28 @@ if (!in_array($gender, $allowed, true)) {
         }
         mysqli_stmt_bind_param($stmt, "sii", $gender, $sno, $admin_id);
         $query1 = mysqli_stmt_execute($stmt);
-       
-     if ($query1 && mysqli_stmt_affected_rows($stmt) > 0) {
-    
-         echo "Gender updated successfully";
-    } 
-    else {
-    
-    echo "No changes made or update failed";
+             $result =0;
+   if (!$query1) {
+
+    echo "<h1 class='msg'>Database update failed.</h1>";
+} elseif (mysqli_stmt_affected_rows($stmt) > 0) {
+    $result =1;
+    echo "<h1 class='msg'>Gender updated successfully. Wait for a few seconds...</h1>";
+} else {
+    echo "<h1 class='msg'>No changes were made. Please select gender</h1>";
 }
- mysqli_stmt_close($stmt);
-        // echo"The id is ".$oldAge."<br>";
-        // echo"And the new gender is ".$newAge;
+         mysqli_stmt_close($stmt);
+         $_SESSION['token'] = bin2hex(random_bytes(32));
         ?>
 
       <script>
-      var up = setTimeout(() => {
-       window.location.href=`../Edit.php?id=<?php echo$sno;?>&updated=gender`;
+            const value = <?php echo $result; ?>;
+            // alert(value)
+     if (value===1) {
+        setTimeout(() => {
+   window.location.href=`../Edit.php?id=<?php echo$sno;?>&updated=gender`;
         }, 5000);
+     }
 
         </script>
      </div>
