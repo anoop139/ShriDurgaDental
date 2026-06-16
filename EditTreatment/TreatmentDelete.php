4717a68f6d0834de6fa6f0c45266c9a3786c7158
Anoop <anoopsrhetty@gmail.com>
2026-06-16 20:44:47 +0530

    Almost secured 8/10

--- EditTreatment/TreatmentDelete.php
+++ EditTreatment/TreatmentDelete.php
@@ -42,75 +42,103 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$n
    <link rel="stylesheet" href="./treatmentDel.css">
 </head>
 <body>
-<form id="mainFom" method="POST">
-<input type="hidden" name="treatId1" value="<?php echo $_POST['treatId'];?>"> 
+<form id="mainFom" method="POST" >
+<input type="hidden" name="treatId1" value="<?php echo $_POST['treatId']; ?>"> 
 <input type="hidden" name="fid1" value="<?php echo $_POST['fid'];?>"> 
 <div id="main-div">
          <?php
           $patientId = $_POST['id'];
-        //   echo"hi ".$patientId  ;
+            //   echo"<h2> Treatment i is $patientId</h2>";
          ?>
         <div class="result-div output">
             <?php
               if (isset($_POST['id']) && isset($_POST['treatId'])) {
                $id = $_POST['id'];
                $tid = $_POST['treatId'];
-            //   echo"<h2> Treatment i is $id</h2>";
+              
 			//   echo"<h2> Treatment fid is $tid</h2>";/
              $getName0 ="select patient.name, treatment.treatment from patient join treatment
              where patient.sno=$id and treatment.tid=$tid"; 
               $query = mysqli_query($conn, $getName0);
              $showName = mysqli_fetch_assoc($query); 
+
                echo"<h1>Are you sure you want to delete treatment record of  "."$showName[name]"."?</h1>";
                echo"<h1 class='dispArea'>Treatment name :  "."$showName[treatment]"."?</h1>";///
+            //    /echo"<h1 class='dispArea'>Treatment name :  "."$showName[tm]"."?</h1>";///
               } 
               if (isset($_POST['deleteTreatment'])) {
              if (isset($_POST['treatId'])) {
              $treatmentId =$_POST['treatId'];
              $patientId =$_POST['primary'];
-             echo"<h1>id".$patientId."</h1>";
-             $deleteTreat ="delete from treatment where tid=$treatmentId";
-             $deleteTreatQuery = mysqli_query($conn, $deleteTreat);
-             if ($deleteTreatQuery) {
-            echo"<h1>Deleted only 1 new inside treatid</h1>";//
-             echo "<script  nonce='$nonce'>
+
+             echo"<h1>patient id  "."$patientId"."</h1>";
+             echo"<h1>treat id  "."$treatmentId"."</h1>";
+            //  if (empty($treatmentId)) {
+            //      echo"<h1>treat id  not set"."</h1>";
+            //  }
+           if (isset($treatmentId)) {
+            $deleteTreat ="delete from treatment where tid=$treatmentId";
+            $deleteTreatQuery = mysqli_query($conn, $deleteTreat);
+            if ($deleteTreatQuery) {//////
+           echo "<script  nonce='$nonce'>
             window.location.href='../TreatmentDetail.php?id=$patientId&Delete=$deleteTreatQuery';
              //</script>"; //pass foreign key
-              } 
-              else {
+              }// /
+            //   else {
              # code...
-             echo"<h1>Deletion failed </h1>";
-               }
-             }
-    
-                   }
-///              }
-            // }
-        if (isset($_GET['fid'])) {
+            //  echo"<h1>D//.eletion failed one </h1>";
+            //    }
+             }//
+           }
+      if (isset($patientId) && empty($treatmentId)) {
+                     echo"<h1>treatment id "."$patientId is set only"."</h1>";
+
+        $deleteAllTreat ="delete from treatment where sno=$patientId";
+          $deleteAllQuery=mysqli_query($conn, $deleteAllTreat);
+          if ($deleteAllQuery) {
+                echo "<script  nonce='$nonce'>
+            window.location.href='../TreatmentDetail.php?id=$patientId&Delete=$deleteTreatQuery';
+             //</script>"; //pass foreign key
+          }
+//             else {
+//              # code...
+//              echo"<h1>Deletion failed all field </h1>";
+//                }
+//            }
+          
             
-                // echo"else ";
-                $primaryKey = $_GET['fid'];
+    
+                }
+// // /              }
+            }
+        if (isset($_POST['fid'])) {
+            
+                echo"else ";
+                $primaryKey = $_POST['fid'];
+                echo"ho".$primaryKey;
               $getName0 ="select patient.name from patient where sno=$primaryKey"; 
               $query = mysqli_query($conn, $getName0);
              $showName = mysqli_fetch_assoc($query); 
                echo"<h1>Are you sure you want to delete all treatment records of  "."$showName[name]"."?</h1>";
+            //    echo"<h1>Are you sure you want to delete all treatment records of  "."$showName[tm]"."?</h1>";
    } 
-          if (isset($_GET['deleteAllTreatment'])) {
-          # code...
-          $foreignKey = $_GET['fid1'];
-             echo"<h1>Foreign key is  $foreignKey</h1>";
-          $deleteAllTreat ="delete from treatment where sno=$foreignKey";
-          $deleteAllQuery=mysqli_query($conn, $deleteAllTreat);
-          if ($deleteAllQuery) {
-            # code...
-            echo"<h1>Deleted All</h1>";
-             echo "<script>window.location.href='../TreatmentDetail.php?id=$foreignKey&DeleteAll=$deleteAllQuery';</script>"; //pass foreign key
 
-          }
-            else {
-             # code...
-             echo"<h1>Deletion failed </h1>";
-               }
+          if (isset($_POST['deleteTreatmentAll'])) {
+          # code...
+          $foreignKey = $_POST['primary'];///
+             echo"<h1>Foriegn ke".$foreignKey."</h1>";
+        //   $deleteAllTreat ="delete from treatment where sno=$foreignKey";
+        //   $deleteAllQuery=mysqli_query($conn, $deleteAllTreat);
+        //   if ($deleteAllQuery) {
+        //     # code...
+        //     echo"<h1>Deleted All</h1>";
+        //      echo "<script>window.location.href='../TreatmentDetail.php?id=$foreignKey&DeleteAll=$deleteAllQuery';</script>"; //pass foreign key
+
+        //   }
+        //     else {
+        //      # code...
+        //      echo"<h1>Deletion failed all field </h1>";
+        //        }
 
 
          }   
@@ -124,12 +152,15 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$n
     <!-- <input type="hidden" name="name" value="//<?php ///echo$fid;?>"> -->
 
     <input type="hidden" name="treatId" value="<?php echo$_POST['treatId'];?>">
-    <input type="hidden"  id="da" value="<?php echo$_POST['DeleteAll'];?>">
+    <input type="hidden"  id="da"  value="<?php echo$_POST['DeleteAll'];?>">
     <input type="hidden" name="primary" value="<?php echo$patientId;?>">
     <input type="hidden" name="n" value="<?php echo$name5;?>">
+    <input type="hidden" name="no" value="<?php echo$_POST['noOfTreat'];?>">
           </div>   
-    <span id="back"><input type="submit" class="submit" value="Back"></span>
+    <span id="back"><input type="submit" class="submit" value="yes"></span>
 <div id="Yes"><input type="submit" class="submit" name="deleteTreatment" id="deleteTreatmentAll" value="Yes" class="btns"> </div>
- </form>  
+<!-- <div id="Yes"><input type="submit" class="submit" name="deleteTreatmentAll" id="deleteTreatmentAll1" value="YesAll" class="btns"> </div> -->
+</form>  
+<script src="./TreatmentDetail.js"></script>
 </body>
 </html>
