6f3695b503e84ddd4edf5001449acac127d94241
Anoop <anoopsrhetty@gmail.com>
2026-06-17 19:00:52 +0530

    Shifted the code of js

---
+++ EditTreatment/TreatmentDelete.js
@@ -0,0 +1,24 @@
+// onload = ()=>{/
+   let  buttonValue =document.getElementById("deleteTreatment")
+  
+    //  alert("You are del all ")/
+// } 
+    let treatNo =document.getElementById("noOfTreat").value 
+    if (treatNo>1) {
+         buttonValue.value="YesAll"
+         buttonValue.name="deleteAll"
+    }
+  
+document.getElementById("mainFom").onsubmit=()=>{
+ 
+  if (treatNo==1) {
+    // alert("You are del 1")
+    return true;
+  }
+  else{
+  
+       
+    return true;
+  }
+}
+   
