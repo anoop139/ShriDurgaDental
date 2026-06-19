4f75f4a2907a4fe3fd952fe7970c6f6638ae6fc3
Anoop <anoopsrhetty@gmail.com>
2026-06-19 12:02:34 +0530

    Prevent xss

--- EditTreatment/TreatmentDelete.php
+++ EditTreatment/TreatmentDelete.php
@@ -20,14 +20,11 @@ if(!isset($_SESSION['user']) || !isset($_SESSION['admin_id'])){
     exit();
 }
 $admin_id = $_SESSION['admin_id'];
-
-if (isset($_POST['p'])) {
-
-    if (!isset($_POST['token']) || !hash_equals($_SESSION['token'], $_POST['token'])) {
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (!isset($_POST['token']) ||
+        !hash_equals($_SESSION['token'], $_POST['token'])) {
         die("CSRF attack detected");
     }
-// /if (empty($_SESSION['token'])) {
-    // $_SESSION['token'] = bin2hex(random_bytes(32));
 }
 $nonce = base64_encode(random_bytes(16));
 
@@ -39,7 +36,7 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$n
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Treatment Deletion page</title>
-   <link rel="stylesheet" href="./treatmentDel.css">
+   <link rel="stylesheet" href="./treatmentDel.css?v=1">
 </head>
 <body>
 <form id="mainFom" method="POST" >
@@ -49,7 +46,20 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$n
          <?php
           $patientId = $_POST['id'];
           $no = $_POST['noOfTreat'];
-            //   echo"<h2> Treatment i is $no</h2>"; DONE
+        if ($no==1) {// If only one treatment exists, Delete All is equivalent to Delete One.
+          # code..
+
+          // echo"<h2>id ".$_POST['id']."</h2>";
+          unset($_POST['fid']);
+          // echo"<h2>fid ".$_POST['fid']."</h2>";
+        }
+        else if ($no>1) {
+          # code..
+
+          // echo"<h2>id ".$_POST['id']."</h2>";
+          unset($_POST['id']);
+          // echo"<h2>fid ".$_POST['fid']."</h2>";
+        }
          ?>
         <div class="result-div output">
             <?php
@@ -57,64 +67,39 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$n
                $id = $_POST['id'];
                $tid = $_POST['treatId'];
               
-			//   echo"<h2> Treatment fid is $tid</h2>";/
+			  // echo"<h2> Treatment fid is $tid</h2>";///
              $getName0 ="select patient.name, treatment.treatment from patient join treatment
              where patient.sno=$id and treatment.tid=$tid"; 
               $query = mysqli_query($conn, $getName0);
              $showName = mysqli_fetch_assoc($query); 
 
-               echo"<h1>Are you sure you want to delete treatment record of  "."$showName[name]"."?</h1>";
-               echo"<h1 class='dispArea'>Treatment name :  "."$showName[treatment]"."?</h1>";///
+               echo"<h1>Are you sure you want to delete treatment record of  ".htmlspecialchars($showName['name'], ENT_QUOTES, 'UTF-8')."?</h1>";
+               echo"<h1 class='dispArea'>Treatment name :  ".htmlspecialchars($showName['treatment'], ENT_QUOTES, 'UTF-8')."?</h1>";///
             //    /echo"<h1 class='dispArea'>Treatment name :  "."$showName[tm]"."?</h1>";///
               } 
               if (isset($_POST['deleteTreatment'])) {
-             if (isset($_POST['treatId'])) {
+             if (isset($_POST['treatId']) && isset($_POST['primary'])) {
              $treatmentId =$_POST['treatId'];
              $patientId =$_POST['primary'];
-
-             echo"<h1>patient id  "."$patientId"."</h1>";
-             echo"<h1>treat id  "."$treatmentId"."</h1>";
-            //  if (empty($treatmentId)) {
-            //      echo"<h1>treat id  not set"."</h1>";
-            //  }
            if (isset($treatmentId)) {
+                //  echo"<h1>Deleted one you r on line no. 73 and treat id =$treatmentId  </h1>";
             $deleteTreat ="delete from treatment where tid=$treatmentId";
+        
             $deleteTreatQuery = mysqli_query($conn, $deleteTreat);
-            if ($deleteTreatQuery) {//////
-           echo "<script  nonce='$nonce'>
-            window.location.href='../TreatmentDetail.php?id=$patientId&Delete=$deleteTreatQuery';
-               </script>"; //pass foreign key
-              }// /
-            //   else {
+            if ($deleteTreatQuery) {////// execute 1 
+            echo"<h1>Dzeleted one </h1>";
+           header("Location: ../TreatmentDetail.php?id=$patientId&Delete=1");
+           exit();
+          }
+            else {
              # code...
-            //  echo"<h1>D//.eletion failed one </h1>";
-            //    }
+             echo"<h1>Dzeletion failed one </h1>";
+               }
              }//
            }
-      if (isset($_POST["deleteAll"]) ) {
-                     echo"<h1>treatment id "."$patientId is set only"."</h1>";
-
-        // $deleteAllTreat ="delete from treatment where sno=$patientId";
-        //   $deleteAllQuery=mysqli_query($conn, $deleteAllTreat);
-        //   if ($deleteAllQuery) {
-        //         echo "<script  nonce='$nonce'>
-        //     window.location.href='../TreatmentDetail.php?id=$patientId&Delete=$deleteTreatQuery';
-        //      //</script>"; //pass foreign key
-          }
-//             else {
-//              # code...
-//              echo"<h1>Deletion failed all field </h1>";
-//                }
-//            }
-          
-            
-    
-                }
-// // /              }
-            
+           }
         if (isset($_POST['fid'])) {
             
-                echo"else ";
                 $primaryKey = $_POST['fid'];
                 // echo"ho".$primaryKey;
               $getName0 ="select patient.name from patient where sno=$primaryKey"; 
@@ -133,10 +118,10 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$n
           $deleteAllQuery=mysqli_query($conn, $deleteAllTreat);
           if ($deleteAllQuery) {
             # code...
-            // echo"<h1>Deleted All  THIS</h1>";
-            echo "<script nonce='$nonce'>
-            window.location.href='../TreatmentDetail.php?id=$foreignKey&DeleteAll=$deleteAllQuery';
-            </script>"; //pass foreign key
+            echo"<h1>Deleted All  </h1>";
+            // echo "<script nonce='$nonce'>
+            // window.location.href='../TreatmentDetail.php?id=$foreignKey&DeleteAll=$deleteAllQuery';
+            // </script>"; //pass foreign key
 
           }
         //     else {
@@ -156,16 +141,16 @@ header("Content-Security-Policy: default-src 'self'; script-src 'self' 'nonce-$n
     <!-- <input type="hidden" name="name" value="//<?php ///echo$fid;?>"> -->
 
     <input type="hidden" name="treatId" value="<?php echo$_POST['treatId'];?>">
+    <input type="hidden" name="token" value="<?php echo htmlspecialchars($_SESSION['token'], ENT_QUOTES, 'UTF-8'); ?>">
     <input type="hidden"  id="da"  value="<?php echo$_POST['DeleteAll'];?>">
     <input type="hidden" name="primary" value="<?php echo$patientId;?>">
     <input type="hidden" name="n" value="<?php echo$name5;?>">
     <input type="hidden" name="no" id="noOfTreat" value="<?php echo$_POST['noOfTreat'];?>">
           </div>   
-    <span id="back"><input type="submit" class="submit" value="yes"></span>
 <div id="Yes"><input type="submit" class="submit" name="deleteTreatment" id="deleteTreatment" value="Yes" class="btns"> </div>
 <!-- <div id="Yes"><input type="submit" class="submit" name="deleteTreatmentAll" id="deleteTreatmentAll1" value="YesAll" class="btns"> </div> -->
 </form>  
 
-<script src="./TreatmentDelete.js?v=1"></script>
+<script src="./TreatmentDelete.js"></script>
 </body>
 </html>
