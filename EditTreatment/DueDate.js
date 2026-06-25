2aa8928bbd482097910a4ec0bba7b96e46977f36
Anoop <anoopsrhetty@gmail.com>
2026-06-25 15:58:33 +0530

    Changed logic

--- EditTreatment/DueDate.js
+++ EditTreatment/DueDate.js
@@ -19,24 +19,15 @@ let inpt = document.getElementById("input");
                 let currentYear       = currentDate.getFullYear()
                 let toDay       = currentDate.getDate()
                 let thisMonth       = currentDate.getMonth()+1
-           if ((date <= toDay || month < thisMonth) && currentYear == year) {
-   
-            alert("Wrong due date or month");
-                e.preventDefault();
-                       return false;
+     
+                if (year < currentYear || (year == currentYear && month < thisMonth) || (year == currentYear && month == thisMonth && date <= toDay)) {
+    
+                    alert("Wrong due date");
+                    e.preventDefault();
+                  }
 
-                    }
-
-           if (year < currentYear) {
-                   alert("Wrong due year");
-                 e.preventDefault();
-                     return false;
-}
-            else{
-             
-                return true//
-                 
-            }
+           
+            
             }
         }
         oninput =()=>{
